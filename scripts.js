dfd116c2a7e0a5e04ca385b350af76909e1d5ede
AKEROO <155340608+AKEROO@users.noreply.github.com>
2024-01-01 16:11:56 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+// scripts.js
+
+function makeCall(phoneNumber) {
+    window.location.href = "tel:" + phoneNumber;
+}
+
+function sendEmail(email) {
+    window.location.href = 'mailto:' + email;
+}
