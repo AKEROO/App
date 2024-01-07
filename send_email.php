a7fe696eeef2c40e5e899886eac86039b92cbddd
AKEROO <155340608+AKEROO@users.noreply.github.com>
2024-01-07 19:30:56 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -7,28 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // عناوين البريد الإلكتروني
     $emails = array(
-        "Mostefa.Bourouaha@Sonatrach.dz",
-        "Hicham.BOUALI@Sonatrach.dz",
-        "ABDELKADER.ABBA@Sonatrach.dz",
-        "FAYCAL.BAGUI@Sonatrach.dz",
-        "MOHAMMEDTAQIYEDDINE.BELAZGHEM@Sonatrach.dz",
-        "TAYEBI.Abdallah@Sonatrach.dz",
-        "Fouad.ALLAOUA@Sonatrach.dz",
-        "BOUKHRIS.Medjdoub@Sonatrach.dz",
-        "TARIK.MECHAKRA@Sonatrach.dz",
-        "MEBAREK.MAMMERI@Sonatrach.dz",
-        "BOUZIANE.Bachir@Sonatrach.dz",
-        "Abdelkader.Hamlaoui@Sonatrach.dz",
-        "SAID.ATAOUAT@Sonatrach.dz",
-        "Lhacene.Hacene@Sonatrach.dz",
         "MOHAMMEDABDALLAH.AKRABOU@Sonatrach.dz",
-        "MOHAMMEDELHASSAN.CHAFAI@Sonatrach.dz",
         "MUSTAPHA.MAHFOUD@Sonatrach.dz",
-        "ABDELLAH.HAMI@Sonatrach.dz",
-        "AHMED.ABIDI@Sonatrach.dz",
-        "NOUREDDINE.ZAHOUANI@Sonatrach.dz",
-        "Yahia.Meftah@Sonatrach.dz",
-        "ILYAS.SMAHI@Sonatrach.dz"
+        "NOUREDDINE.ZAHOUANI@Sonatrach.dz"
     );
 
     // إعداد PHPMailer
